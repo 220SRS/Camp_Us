41d0da3a7c910be1af5d3febed609ff5e4a837aa
mewluee <yunide2@naver.com>
2023-03-30 00:18:27 +0900

    Fix: 검색페이지 수정
    - 캘린더 위치 수정
    - 탭 메뉴 수정(javascript->jquery)

--- src/main/resources/static/js/searchpage.js
+++ src/main/resources/static/js/searchpage.js
@@ -1,8 +1,8 @@
 $(function(){
-    $('.wrap_section_contents > div').hide();
-    $('.tab_section a').click(function () {
-        $('.wrap_section_contents > div').hide().filter(this.hash).fadeIn();
-        $('.tab_section a').removeClass('active');
+    $('.tabcontent > div').hide();
+    $('.tabnav a').click(function () {
+        $('.tabcontent > div').hide().filter(this.hash).fadeIn();
+        $('.tabnav a').removeClass('active');
         $(this).addClass('active');
         return false;
     }).filter(':eq(0)').click();
