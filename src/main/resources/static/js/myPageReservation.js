b8eabc5492aa489acd8bb96abef96228cc42a191
Yejin <kimyj960911@gmail.com>
2023-03-30 20:50:13 +0900

    Refactor: 변수명 변경

--- src/main/resources/static/js/myPageReservation.js
+++ src/main/resources/static/js/myPageReservation.js
@@ -1,35 +1,74 @@
 // 예약 정보 배열
+// 필요한 info
+// 예약 내역 관련 - 예약id(reservationNo) & 예약일자(date) & 예약금액(price)
+// 캠핑장 정보 관련 - 예약된 캠핑장 id & 캠핑장 대표 이미지(img) & 캠핑장명(campingName)
 const reservationData = [
     {
         img: "../../resources/static/images/together.jpg",
-        date: "2022.02.03",
-        hotel: "Hotel Mariott Paris",
+        reservationDate: "2022.02.03",
+        campingName: "Hotel Mariott Paris",
         reservationNo: "23442",
-        amount: "150000",
+        reservationPrice: "150000",
     },
     {
         img: "../../resources/static/images/together.jpg",
-        date: "2022.03.15",
-        hotel: "Hotel Sofitel Seoul",
+        reservationDate: "2022.03.15",
+        campingName: "Hotel Sofitel Seoul",
         reservationNo: "23532",
-        amount: "120000",
+        reservationPrice: "120000",
     },
     {
         img: "../../resources/static/images/together.jpg",
-        date: "2022.03.15",
-        hotel: "Hotel Sofitel Seoul",
+        reservationDate: "2022.03.15",
+        campingName: "Hotel Sofitel Seoul",
         reservationNo: "23532",
-        amount: "120000",
+        reservationPrice: "120000",
     },
     {
         img: "../../resources/static/images/together.jpg",
-        date: "2022.03.15",
-        hotel: "Hotel Sofitel Seoul",
+        reservationDate: "2022.03.15",
+        campingName: "Hotel Sofitel Seoul",
         reservationNo: "23532",
-        amount: "120000",
+        reservationPrice: "120000",
+    },
+    {
+        img: "../../resources/static/images/together.jpg",
+        reservationDate: "2022.03.15",
+        campingName: "Hotel Sofitel Seoul",
+        reservationNo: "23532",
+        reservationPrice: "120000",
+    },
+    {
+        img: "../../resources/static/images/together.jpg",
+        reservationDate: "2022.03.15",
+        campingName: "Hotel Sofitel Seoul",
+        reservationNo: "23532",
+        reservationPrice: "120000",
+    },
+    {
+        img: "../../resources/static/images/together.jpg",
+        reservationDate: "2022.03.15",
+        campingName: "Hotel Sofitel Seoul",
+        reservationNo: "23532",
+        reservationPrice: "120000",
+    },
+    {
+        img: "../../resources/static/images/together.jpg",
+        reservationDate: "2022.03.15",
+        campingName: "Hotel Sofitel Seoul",
+        reservationNo: "23532",
+        reservationPrice: "120000",
+    },
+    {
+        img: "../../resources/static/images/together.jpg",
+        reservationDate: "2022.03.15",
+        campingName: "Hotel Sofitel Seoul",
+        reservationNo: "23532",
+        reservationPrice: "120000",
     }
-
 ];
+
+
 
 // 예약 정보 카드를 보여주는 함수
 function showReservationData(reservationData) {
@@ -46,12 +85,12 @@ function showReservationData(reservationData) {
               </div>
             </div>
             <div class="col-md-6 col-sm-5">
-              <h3 class="hotel_booking">${data.hotel}<span>${data.date}</span></h3>
+              <h3 class="hotel_booking">${data.campingName}<span>${data.reservationDate}</span></h3>
             </div>
             <div class="col-md-2 col-sm-3">
               <ul class="info_booking">
                 <li><strong>예약번호</strong> ${data.reservationNo}</li>
-                <li><strong>금액</strong> ${data.amount}</li>
+                <li><strong>금액</strong> ${data.reservationPrice}</li>
               </ul>
             </div>
             <div class="col-md-2 col-sm-2">
