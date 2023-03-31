8e752fde0e580cbc4f2ae455afa3d360086de53e
mewluee <yunide2@naver.com>
2023-03-31 23:05:59 +0900

    Fix: 검색페이지 수정
    - 탭 메뉴 수정

--- src/main/resources/static/js/searchpage.js
+++ src/main/resources/static/js/searchpage.js
@@ -7,3 +7,14 @@ $(function(){
         return false;
     }).filter(':eq(0)').click();
 });
+
+
+$(function(){
+    $('.areatabcontent > div').hide();
+    $('.areatabnav a').click(function () {
+        $('.areatabcontent > div').hide().filter(this.hash).fadeIn();
+        $('.areatabnav a').removeClass('active');
+        $(this).addClass('active');
+        return false;
+    }).filter(':eq(0)').click();
+});
