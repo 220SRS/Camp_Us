cb62f55ab8704de4ad1f90f3da0377e09eacc4df
hunm719 <hunm719@naver.com>
2023-04-04 22:18:13 +0900

    Feat -예약정보 클래스 수정 -추가옵션 클래스 수정 -예약하기 버튼 추가

--- src/main/resources/static/js/reservation.js
+++ src/main/resources/static/js/reservation.js
@@ -1,17 +1,27 @@
-function count(type)  {
-  // 결과를 표시할 element
-  const resultElement = document.getElementById('result');
+function count1(type)  {
+  const resultElement = document.getElementById('result1');
 
-  // 현재 화면에 표시된 값
   let number = resultElement.innerText;
 
-  // 더하기/빼기
   if(type === 'plus') {
     number = parseInt(number) + 1;
   }else if(type === 'minus')  {
     number = parseInt(number) - 1;
   }
 
-  // 결과 출력
+  resultElement.innerText = number;
+}
+
+function count2(type)  {
+  const resultElement = document.getElementById('result2');
+
+  let number = resultElement.innerText;
+
+  if(type === 'plus') {
+    number = parseInt(number) + 1;
+  }else if(type === 'minus')  {
+    number = parseInt(number) - 1;
+  }
+
   resultElement.innerText = number;
 }
