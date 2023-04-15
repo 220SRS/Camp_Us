cc64a28d4bd07fb6f24ab8d4e1bdbc4d0928baf8
hunm719 <hunm719@naver.com>
2023-04-15 10:49:10 +0900

    Feat -reservation Entity 클래스 수정 -reservation Controller 클래스의 saveReservation 메서드에 stub객체 리턴 -reservationSaveDto, ReservationService, Option 클래스에서 오류가 나지 않도록 주석처리

--- src/main/java/com/campus/campus/reservation/dto/ReservationSaveDto.java
+++ src/main/java/com/campus/campus/reservation/dto/ReservationSaveDto.java
@@ -3,35 +3,9 @@ package com.campus.campus.reservation.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
-import java.util.List;
 
 @Getter
 @Setter
 public class ReservationSaveDto {
-    @NotNull
-    private Long campId;
 
-    @NotNull
-    private String startDate;
-
-    @NotNull
-    private String endDate;
-
-    @NotNull
-    private Long paymentAmount;
-
-    @NotNull
-    private Long paymentStatus;
-
-    // 예약자 정보
-    @NotNull
-    private String name;
-
-    @NotNull
-    private String phoneNumber;
-
-    // 추가옵션 정보
-    private List<Long> optionIds;
 }
--- src/main/java/com/campus/campus/reservation/service/ReservationService.java
+++ src/main/java/com/campus/campus/reservation/service/ReservationService.java
@@ -1,5 +1,6 @@
 package com.campus.campus.reservation.service;
 
+import com.campus.campus.member.MemberService;
 import com.campus.campus.reservation.entity.Reservation;
 import com.campus.campus.reservation.repository.ReservationRepository;
 import org.springframework.data.domain.Page;
@@ -8,41 +9,36 @@ import org.springframework.stereotype.Service;
 @Service
 public class ReservationService {
     private final MemberService memberService;
-    private final CampService campService;
+//    private final CampService campService; 캠프서비스 가져와야함
     private final ReservationRepository reservationRepository;
 
     public ReservationService(MemberService memberService,
-                              CampService campService,
                               ReservationRepository reservationRepository) {
         this.memberService = memberService;
-        this.campService = campService;
         this.reservationRepository = reservationRepository;
     }
 
     public Reservation saveReservation(Reservation reservation) {
         Reservation reservation1 = new Reservation();
-        reservation1.setStartDate("2023-04-13");
-        reservation1.setEndDate("2023-04-14");
-        reservation1.setPaymentAmount(100000L);
-        reservation1.setPaymentStatus(1L);
+
         reservationRepository.save(reservation1);
 
         return reservation1;
     }
 
-    public Reservation updateReservation(Reservation reservation) {
-        return ;
-    }
-
-    public Reservation findByReservation() {
-        Reservation savedReservation = reservationRepository.findById()
-    }
-
-    public Page<Reservation> findByReservations() {
-
-    }
-
-    public Reservation cancelReservation() {
-
-    }
+//    public Reservation updateReservation(Reservation reservation) {
+//
+//    }
+//
+//    public Reservation findByReservation() {
+//        Reservation savedReservation = reservationRepository.findById()
+//    }
+//
+//    public Page<Reservation> findByReservations() {
+//
+//    }
+//
+//    public Reservation cancelReservation() {
+//
+//    }
 }
