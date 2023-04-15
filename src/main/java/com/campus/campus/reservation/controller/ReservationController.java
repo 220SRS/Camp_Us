54323fd073f638ca40357ec1583d1b2e92665623
진진 <kimyj9609@gmail.com>
2023-04-15 14:01:27 +0000

    2023/04/15 예약 정보 기본 틀 및 save 기능 구현

--- src/main/java/com/campus/campus/reservation/controller/ReservationController.java
+++ src/main/java/com/campus/campus/reservation/controller/ReservationController.java
@@ -1,17 +1,18 @@
 package com.campus.campus.reservation.controller;
 
+import com.campus.campus.mypageReservation.ReservationResponseDto;
 import com.campus.campus.reservation.dto.ReservationSaveDto;
-import com.campus.campus.reservation.dto.ReservationUpdateDto;
 import com.campus.campus.reservation.entity.Reservation;
 import com.campus.campus.reservation.service.ReservationService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
 @RestController
 @RequestMapping("/api/reservations")
@@ -23,58 +24,11 @@ public class ReservationController {
         this.reservationService = reservationService;
     }
 
-    @PostMapping
-    public ResponseEntity saveReservation( @RequestBody ReservationSaveDto reservationSaveDto) {
-        String dateStr = "2023-04-20";
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-mm-dd");
-        LocalDateTime date = LocalDateTime.parse(dateStr, formatter);
-
-        // Reservation 객체 생성
-        Reservation reservation  = new Reservation();
-        reservation.setUserId(1L);
-        reservation.setStoreId(1L);
-        reservation.setMbCnt(6);
-        reservation.setStartDate(date);
-        reservation.setEndDate(date);
-        reservation.setPaymentAmt(150000L);
-        reservation.setUserRequest("예약이 편하네요");
-        reservation.setRsvStatus(Reservation.RsvStatus.RSV_USING);
-        reservation.getRsvDate();
-
-        // 예약 정보 생성
-        Reservation savedReservation = reservationService.saveReservation(reservation);
-
-        // savedReservation의 정보를 응답으로 반환
-        return ResponseEntity.ok(savedReservation);
+    // TODO: 예진 - save reservation
+    @PostMapping("/reservation")
+    public ResponseEntity<ReservationResponseDto> postReservation(@RequestBody ReservationSaveDto reservationSaveDto) {
+        ReservationResponseDto reservationResponseDto = reservationService.postReservation(reservationSaveDto);
+        return ResponseEntity.ok(reservationResponseDto);
     }
 
-//    @PostMapping("")
-//    public ResponseEntity updateReservation(ReservationUpdateDto reservationUpdateDto) {
-//
-//    }
-
-//    @PostMapping("/{reservationId}")
-//    public ResponseEntity findByReservationId(@PathVariable("reservationId") @Positive long reservationId) {
-//        //예약 정보 조회
-//    }
-//
-//    @PostMapping
-//    public ResponseEntity saveReservationUserInfo() {
-//        //예약자 정보 생성
-//    }
-//
-//    @PostMapping
-//    public ResponseEntity findByUserId() {
-//        //예약자 정보 조회
-//    }
-//
-//    @PostMapping
-//    public ResponseEntity saveOptions() {
-//        //추가옵션 선택 정보 생성
-//    }
-//
-//    @PostMapping
-//    public ResponseEntity findByOptions() {
-//        //추가옵션 리스트 조회
-//    }
 }
--- src/main/java/com/campus/campus/reservation/dto/ReservationSaveDto.java
+++ src/main/java/com/campus/campus/reservation/dto/ReservationSaveDto.java
@@ -1,11 +1,26 @@
 package com.campus.campus.reservation.dto;
 
+import com.campus.campus.reservation.entity.Reservation;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Date;
 
 
 @Getter
 @Setter
 public class ReservationSaveDto {
+
+    private Long rsvId; // 예약 id
+    private Long userId;    // user id
+    private Long storeId;   // 캠핑장 id
+    private Integer mbCnt;  // 인원 수
+    private LocalDateTime startDate;  // 시작 날짜
+    private LocalDateTime endDate; // 마지막 날짜
+    private Long paymentAmt; // 결제 금액
+    private String userRequest; // 요청 사항
+    private Reservation.RsvStatus rsvStatus; // 결제 상태
+    private LocalDateTime rsvDate;  // 예약한 날짜
 
 }
