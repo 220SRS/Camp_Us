47d8e63a9d7ed1125842617e49d517f33d43810d
Yejin <kimyj9609@gmail.com>
2023-04-22 22:59:43 +0900

    Feat: 엔티티 매핑 기능 구현
    [ 수정 코드 ]
    - CampBaseInfo
    - Member
    - Reservation
    - Review
    - CampusAppication
    - build.gradle

--- src/main/java/com/campus/campus/member/Member.java
+++ src/main/java/com/campus/campus/member/Member.java
@@ -3,15 +3,16 @@ package com.campus.campus.member;
 import com.campus.campus.member.myPageInfo.dto.updateDto.NameUpdateDto;
 import com.campus.campus.member.myPageInfo.dto.updateDto.NickNameUpdateDto;
 import com.campus.campus.member.myPageInfo.dto.updateDto.PhoneUpdateDto;
+import com.campus.campus.reservation.entity.Reservation;
+import com.campus.campus.review.Review;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -23,25 +24,34 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long memberId;
 
+    @Column(length = 30, nullable = false)
     private String name;
-
+    @Column(length = 30, nullable = false)
     private String nickName;
-
+    @Column(nullable = false, updatable = false, unique = true)
     private String email;
-
+    @Column(length = 13, nullable = false, unique = true)
     private String phone;
 
-    public static Member of(MemberSaveDto memberSaveDto) {
+    // Reservation 매핑
+    @OneToMany(mappedBy = "member")
+    private List<Reservation> reservationList = new ArrayList<>();
 
-        Member member = new Member();
+    // Review 매핑
+    @OneToMany(mappedBy = "member")
+    private List<Review> reviewList = new ArrayList<>();
 
-        member.setName(memberSaveDto.getName());
-        member.setNickName(memberSaveDto.getNickName());
-        member.setEmail(memberSaveDto.getEmail());
-        member.setPhone(memberSaveDto.getPhone());
-
-        return member;
-    }
+//    public static Member of(MemberSaveDto memberSaveDto) {
+//
+//        Member member = new Member();
+//
+//        member.setName(memberSaveDto.getName());
+//        member.setNickName(memberSaveDto.getNickName());
+//        member.setEmail(memberSaveDto.getEmail());
+//        member.setPhone(memberSaveDto.getPhone());
+//
+//        return member;
+//    }
 
     public void updateName(NameUpdateDto nameUpdateDto) {
         this.name = nameUpdateDto.getReservation_name();
