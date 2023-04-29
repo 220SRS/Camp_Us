892ff18bece24613117185fcc8659b6f883331bf
mewluee <yunide2@naver.com>
2023-04-29 23:58:33 +0900

    Feat : 검색페이지 기능 구현 작업중
    - 데이터에서 데이터를 분류해서 출력하는 메서드 구현
    - 어떤 데이터가 있는지 확인하는 작업
    - 분류할 데이터 정리 작업중

--- src/main/java/com/campus/campus/search/controller/SearchController.java
+++ src/main/java/com/campus/campus/search/controller/SearchController.java
@@ -2,15 +2,19 @@ package com.campus.campus.search.controller;
 
 import com.campus.campus.dataapi.entity.CampBaseInfo;
 import com.campus.campus.search.dto.AllStoresResponseDto;
+import com.campus.campus.search.dto.CampTypeListDto;
 import com.campus.campus.search.dto.SearchStoreResponseDto;
-import com.campus.campus.search.entity.SearchStore;
 import com.campus.campus.search.service.SearchService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RequestMapping("/search")
 @RestController
@@ -34,6 +38,35 @@ public class SearchController {
         return new ResponseEntity(HttpStatus.OK);
     }
 */
+    @GetMapping("/dataDeduplication")
+    public ResponseEntity dataDeduplication(){
+        List<CampBaseInfo> campBaseInfos = searchService.findAll();
+
+        //내가 하고싶었던 작업
+        //캠프 리스트의 10번 인덱스 값(String)을 따로 배열로 만들기 -> 해당 배열로 해쉬맵 만들기
+//        Map<String, Integer> datas = campBaseInfos.stream().filter(e->)
+//                collect(Collectors.toList())
+        //반복문 말고 스트림써보기
+        System.out.println("dtD");
+
+        Map<String, Integer> categoryDatas=new HashMap<>();
+        Map<String, Integer> AmenityDatas = new HashMap<>();
+        String[] strs;
+
+        for (int i = 0; i < campBaseInfos.size(); i++) {
+            strs = campBaseInfos.get(i).getCategory().split(",");
+            Stream.of(strs).forEach(e -> categoryDatas.compute(e, (k, v) -> v == null ? 1 : v + 1));
+
+            strs = campBaseInfos.get(i).getAmenities().split(",");
+            Stream.of(strs).forEach(e -> AmenityDatas.compute(e, (k, v) -> v == null ? 1 : v + 1));
+
+
+        }
+
+        CampTypeListDto response=new CampTypeListDto(categoryDatas,AmenityDatas);
+
+        return new ResponseEntity(response, HttpStatus.OK);
+    }
 
     //테스트 완료!
     @GetMapping("/camps")
@@ -46,6 +79,10 @@ public class SearchController {
         }*/
         AllStoresResponseDto response = new AllStoresResponseDto(campBaseInfos);
 
+
+        //그런데 모든 데이터를 가져오는게 올바른 걸까..?
+        //일부 데이터 먼저가저오고 스크롤이 내려갈때마다 더 데이터를 가져오는게 더 좋지 않을까..?
+        //그 부분은 더 찾아봐야겠다.
         return new ResponseEntity(response,HttpStatus.OK);
     }
 
---
+++ src/main/java/com/campus/campus/search/dto/CampTypeListDto.java
@@ -0,0 +1,17 @@
+package com.campus.campus.search.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.Map;
+
+//getter없으니까 데이터가 제대로 안들어간다.
+@Getter
+@AllArgsConstructor
+public class CampTypeListDto {
+    Map<String, Integer> campType;
+    Map<String, Integer> amenityType;
+}
